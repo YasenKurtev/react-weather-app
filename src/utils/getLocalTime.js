348f68b14436ec5e25a280c710832a2f079667be
Yasen Kurtev <83857426+YasenKurtev@users.noreply.github.com>
2022-09-10 01:55:34 +0300

    Created get local time function

---
+++ src/utils/getLocalTime.js
@@ -0,0 +1,10 @@
+export default function getLocalTime(offset) {
+    let date = new Date();
+    let localTime = date.getTime();
+    let localOffset = date.getTimezoneOffset() * 60000;
+    let utc = localTime + localOffset;
+    let city = utc + (1000 * -offset);
+    let result = new Date(city);
+
+    return result;
+}
