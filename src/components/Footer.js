ad34e357f78f882615258c41fa2b126a061853c5
Yasen Kurtev <83857426+YasenKurtev@users.noreply.github.com>
2022-09-11 01:06:39 +0300

    Updated footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,24 @@ import { StyledFooter } from "./styles/Footer.styled"
 let Footer = () => {
     return (
         <StyledFooter>
-            <p>Yasen</p>
+            <p className="dev"><i>Developed by Yasen Kurtev 2022</i></p>
+            <div className="social-container">
+                <p className="contact"><i>Contact me:</i></p>
+                <ul className="social-list">
+                    <li className="social-item">
+                        <a className="social-link" href="https://github.com/YasenKurtev"><i className="fa-brands fa-github"></i></a>
+                    </li>
+                    <li className="social-item">
+                        <a className="social-link" href="mailto:yasenkurtev1@gmail.com"><i className="fa-solid fa-envelope"></i></a>
+                    </li>
+                    <li className="social-item">
+                        <a className="social-link" href="https://www.facebook.com/yasen.kurtev.3/"><i className="fa-brands fa-facebook"></i></a>
+                    </li>
+                    <li className="social-item">
+                        <a className="social-link" href="https://www.instagram.com/yasenkurtev10/"><i className="fa-brands fa-instagram"></i></a>
+                    </li>
+                </ul>
+            </div>
         </StyledFooter>
     )
 }
--- src/components/styles/Footer.styled.js
+++ src/components/styles/Footer.styled.js
@@ -2,13 +2,49 @@ import styled from "styled-components";
 
 export let StyledFooter = styled.footer`
     background-color: ${props => props.theme.backgroundColor};
+    color: ${props => props.theme.textColor};
     display: flex;
     flex-direction: row;
-    justify-content: center;
+    justify-content: space-between;
     align-items: center;
     padding: 10px 30px;
     border-top-left-radius: 10px;
     border-top-right-radius: 10px;
     margin-top: 20px;
     height: 3.3vh;
+
+    .dev{
+        font-size: 18px;
+    }
+
+    .contact{
+        font-size: 18px;
+        margin: 0;
+    }
+
+    ul{
+        list-style: none;
+        display: flex;
+        justify-content: space-between;
+        gap: 10px;
+        margin: 0;
+        padding: 0;
+    }
+
+    .social-item i{
+        font-size: 25px;
+        padding: 5px;
+        color: ${props => props.theme.iconsColor};
+    }
+
+    .social-item i:hover{
+        border-radius: 50px;
+        background-color: ${props => props.theme.selectedIconBackgroundColor};
+    }
+
+    .social-container{
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        gap: 10px;
 `
