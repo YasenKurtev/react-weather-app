514241dbcae691622a40d89d2a4c3e4e3e96b339
Yasen Kurtev <83857426+YasenKurtev@users.noreply.github.com>
2022-09-14 12:49:28 +0300

    Updated Loading mini styles

--- src/components/styles/LoadingMini.styled.js
+++ src/components/styles/LoadingMini.styled.js
@@ -1,41 +1,42 @@
 import styled from "styled-components";
 
 export let StyledLoadingMini = styled.div`
-display: flex;
-flex-direction: column;
-align-items: center;
-margin: auto;
-gap: 10px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin: 20px;
+    gap: 10px;
 
-p{
-    color: ${props => props.theme.textColor};
-    font-size: 20px;
-    font-weight: 600;
-    margin: 0;
-}
+    .lds-dual-ring {
+        display: inline-block;
+        width: 80px;
+        height: 80px;
+        }
+        
+        .lds-dual-ring:after {
+        content: " ";
+        display: block;
+        width: 50px;
+        height: 50px;
+        margin: 8px;
+        border-radius: 50%;
+        border: 6px solid ${props => props.theme.textColor};
+        border-color: ${props => props.theme.textColor} transparent ${props => props.theme.textColor} transparent;
+        animation: lds-dual-ring 1.2s linear infinite;
+        }
+        @keyframes lds-dual-ring {
+        0% {
+            transform: rotate(0deg);
+        }
+        100% {
+            transform: rotate(360deg);
+        }
+    }
 
-.lds-dual-ring {
-    display: inline-block;
-    width: 80px;
-    height: 80px;
-  }
-  .lds-dual-ring:after {
-    content: " ";
-    display: block;
-    width: 50px;
-    height: 50px;
-    margin: 8px;
-    border-radius: 50%;
-    border: 6px solid ${props => props.theme.textColor};
-    border-color: ${props => props.theme.textColor} transparent ${props => props.theme.textColor} transparent;
-    animation: lds-dual-ring 1.2s linear infinite;
-  }
-  @keyframes lds-dual-ring {
-    0% {
-      transform: rotate(0deg);
+    p{
+        font-size: 20px;
+        font-weight: 600;
+        margin: 0;
+        color: ${props => props.theme.textColor};
     }
-    100% {
-      transform: rotate(360deg);
-    }
-  }
 `
