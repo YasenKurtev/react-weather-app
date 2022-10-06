8cc2a90a18f69dfa16c87a79e80101fa9064e4c6
Yasen Kurtev <83857426+YasenKurtev@users.noreply.github.com>
2022-10-06 23:48:30 +0300

    Updated Header tests

--- src/components/Header.test.js
+++ src/components/Header.test.js
@@ -2,11 +2,24 @@ import { render, screen } from '@testing-library/react';
 import Header from './Header.js';
 import App from '../App.js';
 
-describe('Header component', () => {
-    test('renders app title', () => {
-        render(<App><Header /></App>);
+test('Renders Header component', () => {
+    render(<App><Header /></App>);
 
-        let appTitleElement = screen.getByText('WeatherApp');
-        expect(appTitleElement).toBeInTheDocument();
-    })
-})
+    let appTitleElement = screen.getByText('WeatherApp');
+    expect(appTitleElement).toBeInTheDocument();
+
+    let locationButton = screen.getByText('Get location');
+    expect(locationButton).toBeInTheDocument();
+
+    let myCitiesButton = screen.getByText('My cities');
+    expect(myCitiesButton).toBeInTheDocument();
+
+    let searchForm = screen.getByPlaceholderText('Search for a city...');
+    expect(searchForm).toBeInTheDocument();
+
+    let unitsButtons = screen.getByText('Units');
+    expect(unitsButtons).toBeInTheDocument();
+
+    let themeButtons = screen.getByText('Dark mode');
+    expect(themeButtons).toBeInTheDocument();
+});
