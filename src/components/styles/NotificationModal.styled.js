76434e44839c1034251ce25a6dac971666468ef5
Yasen Kurtev <83857426+YasenKurtev@users.noreply.github.com>
2022-12-02 23:34:02 +0200

    Updated Notification Modal animation

--- src/components/styles/NotificationModal.styled.js
+++ src/components/styles/NotificationModal.styled.js
@@ -2,12 +2,14 @@ import styled from "styled-components";
 
 export let StyledNotificationModal = styled.div`
     position: fixed;
+    z-index: 1;
     top: 4%;
     left: 50%;
     transform: translate(-50%, -50%);
     background-color: green;
     width: 30%;
     border-radius: 20px;
+    animation: fadeIn 3s forwards;
 
     p{
         margin:0;
@@ -16,4 +18,17 @@ export let StyledNotificationModal = styled.div`
         text-align: center;
         font-style: italic;
     }
+
+    @keyframes fadeIn {
+        0%{
+            opacity: 0;
+        }
+        20%, 50%, 80%{
+            opacity: 1;
+        }
+        100%{
+            opacity: 0;
+            visibility: hidden;
+        }
+      }
 `
