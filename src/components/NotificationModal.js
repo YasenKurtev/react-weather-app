7fd21ca6b51471db03f5e4742d6168d42d9391cf
Yasen Kurtev <83857426+YasenKurtev@users.noreply.github.com>
2022-12-08 23:27:17 +0200

    Updated notification messages

--- src/components/NotificationModal.js
+++ src/components/NotificationModal.js
@@ -5,9 +5,9 @@ let NotificationModal = (props) => {
         <StyledNotificationModal>
             {props.city && <p>Successfully fetched weather data for {props.city}!</p>}
             {props.error && <p>Failed to fetch weather data!</p>}
-            {props.addedCity && <p>Successfully added {props.addedCity} to My Cities!</p>}
-            {props.removedCity && <p>Successfully removed {props.removedCity} from My Cities!</p>}
-            {props.defaultCity && <p>Successfully changed your default city to {props.defaultCity}!</p>}
+            {props.addedCity && <p>Added {props.addedCity} to My Cities!</p>}
+            {props.removedCity && <p>Removed {props.removedCity} from My Cities!</p>}
+            {props.defaultCity && <p>Default city set to {props.defaultCity}!</p>}
         </StyledNotificationModal>
     )
 }
